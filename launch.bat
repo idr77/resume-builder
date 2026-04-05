87f0ea0138f3cc757560c2c7013a579d4041da92
Eric Diallo <diallo.eric@gmail.com>
2026-04-05 20:38:24 +0200

    Add scripts to launch the application

---
+++ launch.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Starting the Resume Builder application locally...
+start http://localhost:5173
+npm run dev
+pause
---
+++ launch.ps1
@@ -0,0 +1,3 @@
+Write-Host "Starting the Resume Builder application locally..." -ForegroundColor Green
+Start-Process "http://localhost:5173"
+npm run dev
